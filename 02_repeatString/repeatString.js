1ab7cf5564e6c01ba2b6dd0d117d567b5ba52168
chalkemandar71 <chalkemandar71@gmail.com>
2023-01-23 15:35:02 +0530

    Solved Exercise.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string, times) {
+    let emptyString = "";
+    if (times < 0) {
+        return "ERROR"
+    } else {
+    while (times > 0) {
+        emptyString += string;
+        times--;
+    }
+    return emptyString;
+}
 };
 
 // Do not edit below this line
