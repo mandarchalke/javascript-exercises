2b6c12d94af8c490390343167dc71043d5e3eed5
chalkemandar71 <chalkemandar71@gmail.com>
2023-01-24 10:31:38 +0530

    Solved exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let newStringArray = string.split("",string.length); // converts string into array
+    let reversedNewStringArray = newStringArray.reverse(); // reverses array
+    let reversedString = reversedNewStringArray.join(""); // joins array 
+    return reversedString; 
 };
 
 // Do not edit below this line
