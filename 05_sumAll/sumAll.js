edecb18279923d017a327aafddd0000d8029ffe8
chalkemandar71 <chalkemandar71@gmail.com>
2023-02-02 10:47:41 +0530

    Solved Exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function (a, b) {
+    if (typeof a !== "number" || typeof b !== "number") {
+        return "ERROR";
+    } else if (a < 0 || b < 0) {
+        return "ERROR";
+    } else if (a < b) {
+        let sum = 0;
+        for (let i = a; i <= b; i++) {
+            sum = sum + i;
+        }
+        return sum;
+    } else {
+        let sum = 0;
+        for (let i = b; i <= a; i++) {
+            sum = sum + i;
+        }
+        return sum;
+    }
 };
 
 // Do not edit below this line
