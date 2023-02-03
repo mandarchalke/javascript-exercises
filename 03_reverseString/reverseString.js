695a2d218a538a34105df5bde57c3961b408f352
chalkemandar71 <chalkemandar71@gmail.com>
2023-02-03 11:14:50 +0530

    Added solution for better understanding.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,8 +1,9 @@
-const reverseString = function(string) {
-    let newStringArray = string.split("",string.length); // converts string into array
+const reverseString = function (string) {
+    let newStringArray = string.split("", string.length); // converts string into array
     let reversedNewStringArray = newStringArray.reverse(); // reverses array
     let reversedString = reversedNewStringArray.join(""); // joins array 
-    return reversedString; 
+    return reversedString;
+    //return string.split("").reverse().join("");// This is actual solution where multiple methods are written in one line.
 };
 
 // Do not edit below this line
