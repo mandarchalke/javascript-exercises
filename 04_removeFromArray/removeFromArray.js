eca843890d85bd27f31a8564ab16f49e51c6a820
chalkemandar71 <chalkemandar71@gmail.com>
2023-02-03 11:26:00 +0530

    Added comments for better understanding of code. Also changed arr to array to better understanding.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,11 +1,11 @@
-const removeFromArray = function(arr, ...arg) {
-   arg.forEach((element) => {
-    const index = arr.indexOf(element);
+const removeFromArray = function(array, ...arg) {
+   arg.forEach((element) => { // for each arg element
+    const index = array.indexOf(element); // we are finding index of element which is same as arg inside array.
     if (index > -1) {
-        arr.splice(index, 1);
+        array.splice(index, 1); //here we pass index if found similar arr.splice will remove that element.
     }
    });
-   return arr;
+   return array; // this returns new modified array.
 };
 
 // Do not edit below this line
