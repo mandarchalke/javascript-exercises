4c37bd40c2f24ba454909ef059f927896977c2a9
chalkemandar71 <chalkemandar71@gmail.com>
2023-02-03 14:57:19 +0530

    Added comments for better understanding.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,6 +1,6 @@
-const convertToCelsius = function (temp) {
+const convertToCelsius = function (temp) { // code could have been in one line.
   let ans = (temp - 32) * 5 / 9;
-  return Math.round(ans * 10)/ 10;
+  return Math.round(ans * 10)/ 10; // ans calculation could have been written inside this line making code compact.
 };
 
 const convertToFahrenheit = function (temp) {
